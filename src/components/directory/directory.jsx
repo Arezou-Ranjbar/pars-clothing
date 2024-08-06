0c9ba4502515621f440327181495ed4545f641da
Arezou Ranjbar <arezourne@gmail.com>
2024-08-06 13:08:44 +0330

    directory added.

---
+++ src/components/directory/directory.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./directory.scss";
+import CategoryItem from "../category-item/category-item";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
